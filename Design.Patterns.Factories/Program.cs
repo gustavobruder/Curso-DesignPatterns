98502145911b1bde9fabca1fcfae8639685a16cb
GustavoBabarone <gugabruder@gmail.com>
2020-08-30 14:34:23 -0300

    TODO - Adicionar aulas sobre Factories

---
+++ Design.Patterns.Factories/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Design.Patterns.Factories
+{
+    class Program
+    {
+        static void ExecutarAulas()
+        {
+            Console.WriteLine("\n----- Aula01 -----");
+            // Aula01.Aula01.Executar();
+            
+            Console.WriteLine("\n----- Aula02 -----");
+            // Aula02.Aula02.Executar();
+            
+            Console.WriteLine("\n----- Aula03 -----");
+            // Aula03.Aula03.Executar();
+            
+            // TODO - Adicionar aulas sobre Factories
+        }
+        
+        static void Main(string[] args)
+        {
+            ExecutarAulas();
+            Console.WriteLine("\nQuarto Design Pattern: Factories!");
+        }
+    }
+}
