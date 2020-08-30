706b40340ad3af7b69474a3aeba00da7d017f8e8
GustavoBabarone <gugabruder@gmail.com>
2020-08-30 14:33:45 -0300

    TODO - Adicionar aulas sobre Singletons

---
+++ Design.Patterns.Singletons/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Design.Patterns.Singletons
+{
+    class Program
+    {
+        static void ExecutarAulas()
+        {
+            Console.WriteLine("\n----- Aula01 -----");
+            // Aula01.Aula01.Executar();
+            
+            Console.WriteLine("\n----- Aula02 -----");
+            // Aula02.Aula02.Executar();
+            
+            Console.WriteLine("\n----- Aula03 -----");
+            // Aula03.Aula03.Executar();
+            
+            // TODO - Adicionar aulas sobre Singletons
+        }
+        
+        static void Main(string[] args)
+        {
+            ExecutarAulas();
+            Console.WriteLine("\nTerceiro Design Pattern: Singletons!");
+        }
+    }
+}
